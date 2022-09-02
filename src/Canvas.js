aa519424e08de217d780087d4ee505596adac7af
Jaeheon Shim <git@jaeheonshim.com>
2022-09-02 15:44:49 -0400

    Add drawing canvas

---
+++ src/Canvas.js
@@ -0,0 +1,78 @@
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
+import drawJson from "./drawInfo.json"
+
+const sinDeg = (a) => {
+    return Math.sin(a / 180 * Math.PI);
+}
+
+const cosDeg = (a) => {
+    return Math.cos(a / 180 * Math.PI);
+}
+
+const transform = (x, y, theta) => {
+    return [x * cosDeg(theta) + y * cosDeg(theta + 90),
+            x * sinDeg(theta) + y * sinDeg(theta + 90)];
+}
+
+export default function Canvas(props) {
+    const canvasRef = useRef(null);
+
+    const [showTurtle, setShowTurtle] = useState(false);
+    const [currentFrame, setCurrentFrame] = useState(drawJson.points.length - 1);
+    
+    const drawTurtle = (x, y, heading, context) => {
+        const width = 20;
+        const height = 25;
+
+        const p1 = transform(-width / 2, -height, heading - 90);
+        const p2 = transform(0, -height * 0.7, heading - 90);
+        const p3 = transform(width / 2, -height, heading - 90);
+
+        context.moveTo(x, y);
+        context.lineTo(x + p1[0], y - p1[1]);
+        context.lineTo(x + p2[0], y - p2[1]);
+        context.lineTo(x + p3[0], y - p3[1]);
+        context.lineTo(x, y);
+        context.fill();
+    }
+
+    const toCenterPoint = (x, y, context) => {
+        return [
+            x + context.canvas.width / 2,
+            context.canvas.height / 2 - y
+        ]
+    }
+
+    const drawScene = (context) => {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+        for(let i = 1; i <= currentFrame; ++i) {
+            const prev = drawJson.points[i - 1];
+            const curr = drawJson.points[i];
+
+            context.moveTo(...toCenterPoint(prev.x, prev.y, context));
+            if(curr.draw) {
+                context.lineTo(...toCenterPoint(curr.x, curr.y, context));
+                context.stroke();
+            } else {
+                context.moveTo(...toCenterPoint(curr.x, curr.y, context));
+            }
+        }
+
+        const lastLocation = drawJson.points[currentFrame];
+        if(showTurtle) {
+            drawTurtle(lastLocation.x, lastLocation.y, lastLocation.heading, context);
+        }
+    }
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+
+        context.fillStyle = "#000000";
+        drawScene(context);
+    }, []);
+
+    return <canvas style={{border: "1px solid #e0dede"}} ref={canvasRef} width={drawJson.width} height={drawJson.height} {...props} />
+}
